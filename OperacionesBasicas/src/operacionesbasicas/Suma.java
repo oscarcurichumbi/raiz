cf53efd1c34e58e22a7c1b9615b69e3668496e09
User <User@TOSHIBA>
2015-11-01 11:45:22 -0500

    creacion clase suma

--- OperacionesBasicas/src/operacionesbasicas/Suma.java
+++ OperacionesBasicas/src/operacionesbasicas/Suma.java
@@ -5,10 +5,22 @@
  */
 package operacionesbasicas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author User
  */
 public class Suma {
+      public void sumar()
+    {
+    Scanner entrada=new Scanner(System.in);
     
+    int valor1=entrada.nextInt();
+    int valor2=entrada.nextInt();
+    
+    int suma= valor1+valor2;
+    
+    System.out.println("La suma es: "+suma);
+    }
 }
