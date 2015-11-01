19c4ec053788913d723c35727385aa96945af413
Xtratech <Xtratech@SERVER01-P01>
2015-11-01 10:06:33 -0500

    Primer archivo subido a GitHub

---
+++ OperacionesBasicas/src/operacionesbasicas/Multiplicacion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacionesbasicas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Xtratech
+ */
+public class Multiplicacion {
+
+    public void multi() {
+        Scanner tecla = new Scanner(System.in);
+        int num1, num2, total;
+
+        System.out.println("Ingrese el numero 1");
+        num1 = tecla.nextInt();
+
+        System.out.println("Ingrese el numero 2");
+        num2 = tecla.nextInt();
+
+        total = num1 * num2;
+
+        System.out.println("El resultado es " + total);
+
+    }
+}
---
+++ OperacionesBasicas/src/operacionesbasicas/OperacionesBasicas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacionesbasicas;
+
+/**
+ *
+ * @author Xtratech
+ */
+public class OperacionesBasicas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Multiplicacion m = new Multiplicacion();
+        m.multi();
+    
+    }
+    
+}
