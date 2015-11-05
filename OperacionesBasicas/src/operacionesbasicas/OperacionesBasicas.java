abcb9dab0c5d07818d6eff46cf1bd36725b0c161
SylviG <sylviguanoluisa@hotmail.com>
2015-11-05 09:44:32 -0500

    Update OperacionesBasicas.java

--- OperacionesBasicas/src/operacionesbasicas/OperacionesBasicas.java
+++ OperacionesBasicas/src/operacionesbasicas/OperacionesBasicas.java
@@ -29,7 +29,7 @@ public class OperacionesBasicas {
         
         RestaDesarrollo res = new RestaDesarrollo();
         res.restar();
-        
+        System.out.println("Fin del trabajo")
         
         
     }
